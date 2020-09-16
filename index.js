1cbb5d0fb24ac311e57df49d259cf70b48b9a2db
hsiang <hsiang@borden.com.tw>
2020-09-16 16:34:28 +0800

    dispatcher end and bot leave

--- index.js
+++ index.js
@@ -2,8 +2,8 @@ require('./system/env');
 require('./server');
 const Discord = require('discord.js');
 const bot = new Discord.Client();
-const Music = require('./utils/Music');
-const music = new Music();
+// const Music = require('./utils/Music');
+// const music = new Music();
 
 bot.on('ready', () => {
   console.info('Successfully logged in!');
@@ -12,13 +12,20 @@ bot.on('ready', () => {
 bot.on('message', async (msg) => {
   if (msg.author.bot) return;
   if (!msg.content.includes('咩噗')) return;
-  if (!msg.member.voice.channel) {
+  const voiceChannel = msg.member.voice.channel;
+  if (!voiceChannel) {
     msg.reply('你必須先加入語音頻道');
     return;
   }
 
-  await music.join(msg);
-  music.play(msg);
+  voiceChannel.join()
+    .then((connection) => {
+      const dispatcher = connection.play('./mp3/ball.mp3');
+      dispatcher.on('end', () => {
+        voiceChannel.leave();
+      });
+    })
+    .catch((err) => console.log(err));
 });
 
 bot.login(process.env.DISCORD_BOT_TOKEN);
