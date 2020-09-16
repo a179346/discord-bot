fcda0f213b9245bdcddb582ec77fee71f70882e2
hsiang <hsiang@borden.com.tw>
2020-09-16 16:04:26 +0800

    init

---
+++ index.js
@@ -0,0 +1,24 @@
+require('./system/env');
+const Discord = require('discord.js');
+const bot = new Discord.Client();
+const Music = require('./utils/Music');
+const music = new Music();
+
+
+bot.on('ready', () => {
+  console.info('Successfully logged in!');
+});
+
+bot.on('message', async (msg) => {
+  if (msg.author.bot) return;
+  if (!msg.content.includes('咩噗')) return;
+  if (!msg.member.voice.channel) {
+    msg.reply('你必須先加入語音頻道');
+    return;
+  }
+
+  await music.join(msg);
+  music.play(msg);
+});
+
+bot.login(process.env.DISCORD_BOT_TOKEN);
---
+++ system/env.js
@@ -0,0 +1,8 @@
+process.env.NODE_TLS_REJECT_UNAUTHORIZED = process.env.NODE_TLS_REJECT_UNAUTHORIZED || '0';
+try {
+  require('./secret');
+} catch (error) {
+  process.env.DISCORD_CLIENT_ID = process.env.DISCORD_CLIENT_ID || 'discord-client-id';
+  process.env.DISCORD_CLIENT_SECRET = process.env.DISCORD_CLIENT_SECRET || 'discord-client-secret';
+  process.env.DISCORD_BOT_TOKEN = process.env.DISCORD_BOT_TOKEN || 'discord-bot-token';
+}
